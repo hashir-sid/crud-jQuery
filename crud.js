499988363daa5ad5deaf15a7ab00efac2a170104
hashir-sid <hashirazeem1010@gmail.com>
2023-11-08 22:36:26 +0500

    crud

---
+++ crud.js
@@ -0,0 +1,68 @@
+let url = "https://jsonplaceholder.typicode.com/todos/";
+// url: "https://usman-cui-recipies.herokuapp.com/api/recipes/",
+
+let onSubmit = () => {
+  $("#myForm").on("click", function (e) {
+    // e.prevntDefault();
+
+    if ($("input").val() != "") {
+      console.log("input is not empty");
+
+      var title = $("#title").val();
+      var body = $("#body").val();
+
+      $.ajax({
+        url: url,
+        method: "POST",
+        data: { title, body },
+        success: function (res) {
+          console.log(res);
+        },
+      });
+    } else {
+      console.log("input is empty");
+    }
+  });
+};
+
+let loadGET = () => {
+  $.ajax({
+    url: url,
+    method: "GET",
+    error: function (error) {
+      console.log("No");
+      console.log(error);
+    },
+    success: function (response) {
+      console.log(response);
+    },
+  });
+};
+
+let loadPOST = () => {
+  var title = $("#title").val();
+  var body = $("#body").val();
+
+  $.ajax({
+    url: url,
+    method: "POST",
+    data: { title, body },
+    success: function (response) {
+      console.log(response);
+    },
+  });
+};
+
+let loadDELETE = () => {
+  $.ajax({
+    url: url + "1",
+    method: "DELETE",
+    error: function (error) {
+      console.log("No");
+      console.log(error);
+    },
+    success: function (response) {
+      console.log(response);
+    },
+  });
+};
